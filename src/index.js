6337b172ee7cadd9d40d95ae1a495a9cceb94264
wednesday_developer <wednesday.developer@gmail.com>
2017-11-23 08:49:15 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+//import { Values } from "redux-form-website-template";
+import store from "./store";
+import showResults from "./showResults";
+import SimpleForm from "./SimpleForm";
+
+const rootEl = document.getElementById("content");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <div style={{ padding: 15 }}>
+      <h2>Simple Form</h2>
+      <SimpleForm onSubmit={showResults} />
+    </div>
+  </Provider>,
+  rootEl
+);
